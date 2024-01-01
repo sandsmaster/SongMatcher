7efe35bbfafd57d69f7d7f02fbf6eaafd65ccef9
sandsmaster <www_123_val@abv.bg>
2024-01-01 21:49:36 +0100

    Add unit tests for functions
    	- del_extension
    	- sort_scores

--- test_SongMatcher.py
+++ test_SongMatcher.py
@@ -1,10 +1,23 @@
 from SongMatcher import SongMatcherGame, del_extension, is_correct_guess, sort_scores
+from pytest import raises
 
 
-def test_del_extension():
+def test_base_del_extension():
     assert del_extension("Hello.mp3") == "Hello"
     assert del_extension("Hello.wav") == "Hello"
     assert del_extension("Hello.man.wav") == "Hello.man"
+
+
+def test_type_del_extension():
+    with raises(TypeError):
+        assert del_extension(1)
+        assert del_extension(None)
+
+
+def test_format_del_extension():
+    with raises(IndexError):
+        assert del_extension("Hello")
+        assert del_extension("Hi My Friends.")
 
 
 def test_is_correct_guess():
@@ -13,7 +26,7 @@ def test_is_correct_guess():
     assert is_correct_guess("Pi thon", "pi thon") == False
 
 
-def test_sort_scores():
+def test_base_sort_scores():
     scores_f = [            # scores False
         ["Mick", 20],
         ["Don", 10],
@@ -24,7 +37,7 @@ def test_sort_scores():
         ["Boney", 12],
         ["Don", 10]
     ]                
-    assert sort_scores(scores_f) == scores_t
+    assert sort_scores(scores_f, 1) == scores_t
 
     scores_f[0] = ["Mick", 3]  # set Mick's score to 3
     scores_t = [
@@ -32,12 +45,24 @@ def test_sort_scores():
         ["Don", 10],
         ["Mick", 3]
     ]
-    assert sort_scores(scores_f) == scores_t
+    assert sort_scores(scores_f, 1) == scores_t
+
+
+def test_type_sort_scores():
+    with raises(TypeError):
+        assert sort_scores(10, 1)
+        assert sort_scores(False, 1)
+        assert sort_scores(["No list"], 1)
+
+
+def test_empty_sort_scores():
+    with raises(IndexError):
+        assert sort_scores([], 1)
 
                                                        
-def test_get_songs_len():
+def test_len_get_songs():
     assert len(SongMatcherGame(3, 3).get_songs()) == 3
     assert len(SongMatcherGame(3, 1).get_songs()) == 1
 
-def test_get_songs_type():
+def test_type_get_songs():
     assert type(SongMatcherGame(3, 3).get_songs()[0]) == str
